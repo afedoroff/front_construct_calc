09e4749318a10d7a297e11938fca54fba59e1cf7
Alexandr Fedorov <alexandr.fedorov@idp.zyfra.com>
2022-10-02 19:21:59 +0400

    deploy settings

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+app.use(express.json());
 app.use(express.static(__dirname + '/dist/front-construct-calc'));
-app.get('/*', function(res, req){
-    res.send(path.join(__dirname + '/dist/front-construct-calc/index.html'));
-});
 app.listen(process.env.PORT || 8080);
